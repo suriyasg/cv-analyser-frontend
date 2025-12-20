792727ebf77b3880d313df0095be35427bdc24e3
suriyasg <suriyasg2002@gmail.com>
2025-12-20 22:27:22 +0530

    updated ScanResults component according to updated CVScan model
    updated dummy data according to new CVScan model

--- src/components/dashboard/ScanResults.tsx
+++ src/components/dashboard/ScanResults.tsx
@@ -1,19 +1,59 @@
 import { Button } from "@heroui/button";
 import { Progress } from "@heroui/progress";
 import { useEffect, useState } from "react";
-import ReactMarkdown from "react-markdown";
-import { dummyCVScan } from "@/dummy_data";
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import { dummyCVScans } from "@/dummy_data";
 import { api } from "@/util/api";
+
 export interface CVScan {
 	id: number;
+	cv: CV; // reference to the CV object
 	job_description: string;
-	scan_status: string;
-	scan_result: string;
-	overall_match: number;
-	hard_skill_match: number;
-	soft_skill_match: number;
-	experience_level: string;
-	cv: CV;
+	scan_status: "PENDING" | "STARTED" | "PROCESSING" | "FINISHED";
+	// Text outputs
+	scan_result: string; // final summary for UI
+	anonymized_cv_text?: string;
+	preprocessed_cv_text?: string;
+
+	// Skill analysis
+	identified_hard_skills: {
+		extraction_reasoning: string;
+		found_hard_skills: string[];
+	};
+	identified_soft_skills: {
+		extraction_reasoning: string;
+		found_soft_skills: string[];
+	};
+
+	hard_skill_analyser_output: {
+		found_hard_skills: string[];
+		missing_hard_skills: string[];
+		match_score: number;
+		summary: string;
+	};
+
+	soft_skill_analyser_output: {
+		found_soft_skills: string[];
+		missing_soft_skills: string[];
+		match_score: number;
+		summary: string;
+	};
+
+	// Summary generator
+	summary_generator_output: {
+		overall_match: number;
+		strengths: string[];
+		weaknesses: string[];
+		recommendations: string[];
+		final_summary: string;
+		experience_level?:
+			| "Entry-Level"
+			| "Junior"
+			| "Mid-Level"
+			| "Senior"
+			| "Principal/Lead";
+	};
 }
 
 export interface CV {
@@ -23,7 +63,13 @@ export interface CV {
 	owner_id: number;
 	created: Date;
 	modified: Date;
-	cvscan_set: number[];
+	scans: PartialScan[];
+}
+
+export interface PartialScan {
+	id: number;
+	created: Date;
+	scan_status: "PENDING" | "STARTED" | "PROCESSING" | "FINISHED";
 }
 
 interface ScanResultsProps {
@@ -36,18 +82,19 @@ function ScanResults({ scanId, setScanId }: ScanResultsProps) {
 	const [activeScanInputTab, setActiveScanInputTab] = useState<
 		"PDF" | "JOBDESC"
 	>("PDF");
+
 	useEffect(() => {
 		if (!scanId) return;
-		setScan(dummyCVScan);
-		// api
-		// 	.get(`/scan_results/${scanId}`)
-		// 	.then((response) => {
-		// 		console.log(response.data);
-		// 		setScan(response.data);
-		// 	})
-		// 	.catch((error) => {
-		// 		console.log(error);
-		// 	});
+		// setScan(dummyCVScans[scanId - 1]);
+		api
+			.get(`/scans/${scanId}`)
+			.then((response) => {
+				console.log(response.data);
+				setScan(response.data);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
 	}, [scanId]);
 	return (
 		<div className="w-full p-2">
@@ -59,9 +106,7 @@ function ScanResults({ scanId, setScanId }: ScanResultsProps) {
 					Results from AI Agent
 				</p>
 			</div>
-			<p className="text-gray-400 dark:text-gray-400 text-base lg:text-medium font-normal leading-relaxed max-w-2xl">
-				{scan?.cv.title}
-			</p>
+			<h1 className="text-2xl font-bold p-2 m-2">{scan?.cv.title}</h1>
 			<div className="w-full flex flex-row mb-6" id="score-board">
 				<div
 					className="w-1/4 min-w-40 h-30 shadow-md rounded-2xl p-2 m-2 hover:bg-gray-50"
@@ -69,13 +114,17 @@ function ScanResults({ scanId, setScanId }: ScanResultsProps) {
 				>
 					<div className="text-2xl font-bold">Overall Match</div>
 					<div className="text-xl">
-						<span className="font-bold text-2xl">{scan?.overall_match}</span>
+						<span className="font-bold text-2xl">
+							{scan?.summary_generator_output.overall_match}
+						</span>
 						<span className="font-semibold text-gray-500">/100</span>
-						{scan?.overall_match && (
+						{scan?.summary_generator_output.overall_match !== undefined && (
 							<Progress
 								aria-label="Loading..."
-								color={getProgressBarColourByValue(scan?.overall_match)}
-								value={scan?.overall_match}
+								color={getProgressBarColourByValue(
+									scan?.summary_generator_output.overall_match,
+								)}
+								value={scan?.summary_generator_output.overall_match}
 							/>
 						)}
 					</div>
@@ -86,13 +135,17 @@ function ScanResults({ scanId, setScanId }: ScanResultsProps) {
 				>
 					<div className="text-2xl font-bold">Hard skils</div>
 					<div className="text-xl">
-						<span className="font-bold text-2xl">{scan?.hard_skill_match}</span>
+						<span className="font-bold text-2xl">
+							{scan?.hard_skill_analyser_output.match_score}
+						</span>
 						<span className="font-semibold text-gray-500">/100</span>
-						{scan?.hard_skill_match && (
+						{scan?.hard_skill_analyser_output.match_score !== undefined && (
 							<Progress
 								aria-label="Loading..."
-								color={getProgressBarColourByValue(scan?.hard_skill_match)}
-								value={scan?.hard_skill_match}
+								color={getProgressBarColourByValue(
+									scan?.hard_skill_analyser_output.match_score,
+								)}
+								value={scan?.hard_skill_analyser_output.match_score}
 							/>
 						)}
 					</div>
@@ -103,13 +156,17 @@ function ScanResults({ scanId, setScanId }: ScanResultsProps) {
 				>
 					<div className="text-2xl font-bold">Soft skills</div>{" "}
 					<div className="text-xl">
-						<span className="font-bold text-2xl">{scan?.soft_skill_match}</span>
+						<span className="font-bold text-2xl">
+							{scan?.soft_skill_analyser_output.match_score}
+						</span>
 						<span className="font-semibold text-gray-500">/100</span>
-						{scan?.soft_skill_match && (
+						{scan?.soft_skill_analyser_output.match_score !== undefined && (
 							<Progress
 								aria-label="Loading..."
-								color={getProgressBarColourByValue(scan?.soft_skill_match)}
-								value={scan?.soft_skill_match}
+								color={getProgressBarColourByValue(
+									scan?.soft_skill_analyser_output.match_score,
+								)}
+								value={scan?.soft_skill_analyser_output.match_score}
 							/>
 						)}
 					</div>
@@ -120,48 +177,90 @@ function ScanResults({ scanId, setScanId }: ScanResultsProps) {
 				>
 					<div className="text-2xl font-bold">Experience Level</div>
 					<div className="text-xl">
-						<span className="font-bold">{scan?.experience_level}</span>
+						<span className="font-bold">
+							{scan?.summary_generator_output.experience_level || "Unknown"}
+						</span>
 					</div>
 				</div>
 			</div>
 
 			<div className="mb-6" id="keyword-analysis">
-				<h1 className="text-2xl font-bold">Keyword Analysis</h1>
+				<h1 className="text-2xl font-bold">Skills Analysis</h1>
 				<hr className="border-1 border-gray-300 mb-4" />
 				<div className="mb-4">
-					<h6 className="text-medium font-semibold mb-2">Matched Skills</h6>
-					{["react", "typscript"].map((skill) => {
-						return (
-							<span
-								className="bg-green-200 rounded-lg m-1 p-1 text-sm"
-								key={skill}
-							>
-								{skill}
-							</span>
-						);
-					})}
-				</div>
+					<div className="mb-2">
+						<h6 className="text-medium font-semibold mb-2">Hard Skills</h6>
+						<div className="flex flex-wrap font-light">
+							{scan?.hard_skill_analyser_output.found_hard_skills.map(
+								(skill) => {
+									return (
+										<div
+											className="bg-green-200 rounded-lg m-1 p-1 text-sm w-fit"
+											key={skill}
+										>
+											✅ {skill}
+										</div>
+									);
+								},
+							)}
 
-				<div>
-					<h6 className="text-medium font-semibold mb-2">Missing Skills</h6>
-					{["Doker", "Teraform"].map((skill) => {
-						return (
-							<span
-								className="bg-red-200 rounded-lg m-1 p-1 text-sm"
-								key={skill}
-							>
-								{skill}
-							</span>
-						);
-					})}
+							{scan?.hard_skill_analyser_output.missing_hard_skills.map(
+								(skill) => {
+									return (
+										<div
+											className="bg-red-200 rounded-lg m-1 p-1 text-sm w-fit"
+											key={skill}
+										>
+											❌ {skill}
+										</div>
+									);
+								},
+							)}
+						</div>
+					</div>
+					<div>
+						<h6 className="text-medium font-semibold mb-2">Soft Skills</h6>
+						<div className="flex flex-wrap font-light">
+							{scan?.soft_skill_analyser_output.found_soft_skills.map(
+								(skill) => {
+									return (
+										<div
+											className="bg-green-200 rounded-lg m-1 p-1 text-sm w-fit"
+											key={skill}
+										>
+											✅ {skill}
+										</div>
+									);
+								},
+							)}
+							{scan?.soft_skill_analyser_output.missing_soft_skills.map(
+								(skill) => {
+									return (
+										<div
+											className="bg-red-200 rounded-lg m-1 p-1 text-sm w-fit"
+											key={skill}
+										>
+											❌ {skill}
+										</div>
+									);
+								},
+							)}
+						</div>
+					</div>
 				</div>
 			</div>
 
 			<div className="mb-6" id="summay">
 				<h1 className="text-2xl font-bold">Summary</h1>
 				<hr className="border-1 border-gray-300 mb-4" />
-				<ReactMarkdown>{scan?.scan_result}</ReactMarkdown>
-				{/* <p className="text-sm">{scan?.scan_result}</p>  */}
+				<div
+					className="p-4 markdown bg-gray-100 rounded-2xl"
+					id="summary-mark-down"
+				>
+					<Markdown remarkPlugins={[remarkGfm]}>
+						{scan?.summary_generator_output.final_summary}
+					</Markdown>
+				</div>
 			</div>
 
 			<div className="mb-6" id="scan-inputs">
@@ -183,7 +282,6 @@ function ScanResults({ scanId, setScanId }: ScanResultsProps) {
 				</div>
 				{activeScanInputTab === "PDF" && (
 					<div className="w-full p-2" id="cv-file">
-						<h1 className="text-2xl font-bold">Uploaded CV</h1>
 						<object
 							className="h-screen"
 							data="http://localhost:5173/api/media/uploads/Suriya_Gnanamoorthy_Dy3kDRF.pdf"
@@ -203,31 +301,45 @@ function ScanResults({ scanId, setScanId }: ScanResultsProps) {
 
 				{activeScanInputTab === "JOBDESC" && (
 					<div className="w-full p-2" id="job-description">
-						<h1 className="text-2xl font-bold">Job Description</h1>
-						<ReactMarkdown>{scan?.job_description}</ReactMarkdown>
-						{/* <div>{scan?.job_description}</div> */}
+						{/* The problem here is that React-markdown map the markdown text to real html elements, and you're using tailwindcss. 
+                            Tailwind takes out all default styles applied to html elements. Luckily there is a really easy workaround: 
+                            https://stackoverflow.com/questions/74607419/react-markdown-don%C2%B4t-render-markdown
+                            */}
+						<div
+							className="p-4 markdown bg-gray-100 rounded-2xl"
+							id="job-description-mark-down"
+						>
+							<Markdown remarkPlugins={[remarkGfm]}>
+								{scan?.job_description}
+							</Markdown>
+						</div>
 					</div>
 				)}
 			</div>
 
-			<div>
-				other scans
-				{scan?.cv.cvscan_set.map((scanId) => {
-					return (
-						<div key={scanId}>
-							<button
-								className="p-1 bg-blue-500"
-								onClick={() => setScanId(scanId)}
-								type="button"
-							>
-								{scanId}
-							</button>
-						</div>
-					);
-				})}
+			<div className="mb-6" id="keyword-analysis">
+				<h1 className="text-2xl font-bold">Other Scans</h1>
+				<hr className="border-1 border-gray-300 mb-4" />
+				<div className="mb-4">
+					{scan?.cv.scans.map((scan) => {
+						return (
+							<div key={scan.id}>
+								<button
+									className="p-2 bg-blue-300 rounded-lg"
+									onClick={() => {
+										console.log("clieck", scan.id);
+										setScanId(scan.id);
+										window.scrollTo({ top: 0 });
+									}}
+									type="button"
+								>
+									{scan.id}
+								</button>
+							</div>
+						);
+					})}
+				</div>
 			</div>
-			<div className="w-full p-6" id="job-description"></div>
-			<div className="w-full p-6" id="submit"></div>
 		</div>
 	);
 }
