e00937e58c1a5fa9d7ec6bacc21f9d130ad0f504
suriyasg <suriyasg2002@gmail.com>
2025-12-20 22:36:13 +0530

    updated response interceptor to refresh accesstoken if token error occured

--- src/util/api.ts
+++ src/util/api.ts
@@ -24,8 +24,25 @@ api.interceptors.response.use(
 	(response) => response,
 	(error) => {
 		if (error.response?.status === 401) {
-			useAuthStore.getState().logout();
-			window.location.href = "/login";
+			// if access token error try to refresh it
+			if (error.response.data.errors[0].code === "token_not_valid") {
+				const token = useAuthStore.getState().token;
+				const refresh = useAuthStore.getState().refresh;
+				api
+					.post("/api/v1/auth/token/refresh/", {
+						access: token,
+						refresh,
+					})
+					.then((response) => {
+						useAuthStore.getState().setAuth(response.data.access);
+					})
+					.catch(() => {
+						// refreshing the token is also result in error log out user and make them log in again
+						// console.log(error);
+						useAuthStore.getState().logout();
+						window.location.href = "/login";
+					});
+			}
 		}
 		return Promise.reject(error);
 	},
