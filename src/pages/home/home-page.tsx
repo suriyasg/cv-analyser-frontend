9989d153c3fa18e8dfec4202ad63feec2934a187
suriyasg <suriyasg2002@gmail.com>
2025-12-21 08:06:44 +0530

    clean up unused imports and type

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -1,20 +1,5 @@
-import { Button } from "@heroui/button";
-import { useState } from "react";
-import { Link } from "react-router-dom";
 import Hero from "@/components/home/Hero";
 import Navbar from "@/components/home/Navbar";
-import { api } from "@/util/api";
-import { useAuthStore } from "@/util/auth";
-
-interface User {
-	id: string;
-	user_type: string;
-	username: string;
-	email: string;
-	first_name: string;
-	last_name: string;
-}
-
 export const HomePage = () => {
 	return (
 		<div>
